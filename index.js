8c9f2d4e77154abf34d894c78627778c3c81f6b9
sainttobs <sainttobs@gmail.com>
2019-03-19 21:13:15 +0100

    ok

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const express = require('express')
+const express = require('express');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 
-const app = express()
+const app = express();
 
 const routes = require('./routes/index');
 
